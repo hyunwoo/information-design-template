6a877a608075c35fc24e355cb389c503c2071b50
한현우 <hyunwoo@ip-10-10-10-8.ap-northeast-2.compute.internal>
2023-10-19 16:19:39 +0900

    Chart Added

---
+++ src/routes/chart/c3/line/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import c3 from 'c3';
+
+  let ele: HTMLDivElement;
+  $: {
+    console.log({ c3 });
+    let chart = c3.generate({
+      bindto: ele,
+      data: {
+        columns: [
+          ['data1', 30, 200, 100, 400, 150, 250],
+          ['data2', 50, 20, 10, 40, 15, 25],
+        ],
+      },
+    });
+  }
+</script>
+
+<div class="root">
+  <div id="chart" bind:this="{ele}"></div>
+</div>
+
+<style lang="scss">
+  .root {
+    height: 400px;
+    #chart {
+      height: 100%;
+    }
+  }
+</style>
---
+++ src/routes/chart/chartjs/line/+page.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import { Chart } from 'chart.js/auto';
+
+  let eleRoot: HTMLCanvasElement;
+  let chart: Chart;
+  const labels = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL'];
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 205, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(201, 203, 207, 0.2)',
+        ],
+        borderColor: [
+          'rgb(255, 99, 132)',
+          'rgb(255, 159, 64)',
+          'rgb(255, 205, 86)',
+          'rgb(75, 192, 192)',
+          'rgb(54, 162, 235)',
+          'rgb(153, 102, 255)',
+          'rgb(201, 203, 207)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  $: {
+    if (eleRoot) {
+      console.log(eleRoot);
+      chart = new Chart(eleRoot, {
+        type: 'bar',
+        data: data,
+      });
+    }
+  }
+</script>
+
+<div class="root">
+  <canvas class="root" bind:this="{eleRoot}"></canvas>
+</div>
+
+<style lang="scss">
+  .root {
+    height: 400px;
+  }
+</style>
